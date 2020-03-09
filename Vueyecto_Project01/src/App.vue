c4d60908322dd9023fda9f760edfd57b28a151b9
Desiré <44606766+helleworldGIT@users.noreply.github.com>
2020-03-09 10:24:06 +0100

    Vueyecto - First VUE CLI Project/Exercise

---
+++ Vueyecto_Project01/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- La prop "msg" ya está definida dentro del componente, le pasamos un valor -->
+    <HelloWorld msg="¡Cambiamos un componente!"/>
+  </div>
+</template>
+
+<script>
+// Importando el componente que queremos usar, HelloWorld
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vueyecto_Project01/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      Hemos trasteado con un componente para ver la reactividad de Vue.
+    </p>
+    <img src="https://media.giphy.com/media/xUA7aMYThXDVx4y5O0/giphy.gif">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  // La prop msg definida
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* Añadiendo algo de espacio inferior a la imagen GIF */
+img {
+  margin-bottom: 4rem;
+}
+</style>
